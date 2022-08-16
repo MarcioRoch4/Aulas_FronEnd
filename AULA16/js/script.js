ef26c2acc1fc57eef98745e8ec08271d57a65ec2
Aluno noite <alunonoite@senai.ti>
2022-08-15 21:32:03 -0300

    AULA 15/08

---
+++ AULA16/js/script.js
@@ -0,0 +1,22 @@
+let nome ="Rafael";
+let nota1Rafael = 10;
+let nota2Rafael = 6;
+var soma
+var media
+
+soma = nota1Rafael + nota2Rafael;
+media = soma / 2;
+
+if (media > 6){
+	alert("Aprovado");
+	
+} else if (media < 6){
+	alert("Reprovado");
+	
+}else{
+	alert("Passou raspando")
+}
+
+console.log(media);
+
+
---
+++ AULA16/js/script1.js
@@ -0,0 +1,39 @@
+var primeiroNumero;
+var segundoNumero;
+var soma;
+var media;
+
+let texto1 = "Esse é o meu primeiro texto";
+var texto2 = "Esse é o meu segundo texto";
+const texto3 = "Esse é o meu terceiro texto";
+
+let indicador = 1;
+let nome ="Marcio"
+let idade ="29"
+
+let x = 10;
+let y = 10;
+
+if(x > y){
+	alert("X é maior que Y");
+
+} else if(x < y){
+	alert("Y é maior que X");
+
+}else{(x == y)
+	alert("X é igual a Y");
+}
+
+primeiroNumero = 10;
+segundoNumero = 7;
+soma = primeiroNumero + segundoNumero;
+media = soma / 2
+
+console.log(primeiroNumero);
+console.log(segundoNumero);
+console.log(soma);
+console.log(media);
+console.log(primeiroNumero * segundoNumero);
+console.log(texto1 + texto2 + texto3); 
+console.log(indicador + texto1);
+console.log("A idade de " +nome + " é: " + idade);
