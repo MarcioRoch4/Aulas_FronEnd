4868b1f08bbe2e5b0a6dd2728dee15a09681c520
MarcioRoch4 <marcio.c.freitas@aluno.senai.br>
2022-08-16 09:46:06 -0300

    CORRIGIDO

--- AULA16/js/script.js
+++ AULA16/js/script.js
@@ -1,11 +1,25 @@
-let nome ="Rafael";
+let nome =("Rafael", "Pedro", "Joao");
 let nota1Rafael = 10;
 let nota2Rafael = 6;
+let nota1Pedro = 5;
+let nota2Pedro = 6
+let nota1Joao = 5;
+let nota2Joao = 7;
 var soma
 var media
+var soma2
+var media2
+var soma3
+var media3
 
 soma = nota1Rafael + nota2Rafael;
 media = soma / 2;
+
+som2 = nota1Pedro + nota2Pedro;
+media2 = soma2 / 2;
+
+soma3 = nota1Joao + nota2Joao;
+media3 = soma3 / 2;
 
 if (media > 6){
 	alert("Aprovado");
@@ -17,6 +31,30 @@ if (media > 6){
 	alert("Passou raspando")
 }
 
+if (media2 > 6){
+	alert("Aprovado");
+	
+} else if (media2 < 6){
+	alert("Reprovado");
+	
+}else{
+	alert("Passou raspando")
+}
+
+if (media3 > 6){
+	alert("Aprovado");
+	
+} else if (media3 < 6){
+	alert("Reprovado");
+	
+}else{
+	alert("Passou raspando")
+}
+
 console.log(media);
+console.log(media2);
+console.log(media3);
+
+
 
 
